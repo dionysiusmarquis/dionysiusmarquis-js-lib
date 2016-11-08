25db75e17a431729baa9ad1f386dbf968b351ab8
Dennis Markgraf <info@dennismarkgraf.com>
2016-11-08 20:24:49 +0100

    imageloader fixes

--- src/js/dm/core/ImageLoader.js
+++ src/js/dm/core/ImageLoader.js
@@ -3,22 +3,33 @@
  * @requires dm/core/EventTarget.js
  */
 
-dm.ImageLoader = function() {
+dm.ImageLoader = function(autoStart, invalidateAll) {
 	dm.EventTarget.call(this);
 
 	var self = this;
 	var images = new Object();
 
-	detectSrcChange();
+	if(autoStart !== false)
+		detectSrcChange();
 
-	function imageLoadHandler(event) {
-		if(!self.isLoading()) {
-			self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_LOAD));
+	function imageHandler(event) {
+		switch(event.type) {
+			case dm.ImageLoaderImage.EVENT_ERROR:
+				self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_ERROR, event.target));
+				break;
+
+			case dm.ImageLoaderImage.EVENT_LOAD:
+				self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_IMAGE_LOAD, event.target));
+
+				if(!self.isLoading())
+					self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_LOAD));
+				break;
 		}
 	}
 
 	function detectSrcChange() {
-		requestAnimationFrame(detectSrcChange);
+		if(images)
+			requestAnimationFrame(detectSrcChange);
 
 		var src, image, imageElement;
 		for(src in images) {
@@ -27,18 +38,27 @@ dm.ImageLoader = function() {
 			if(imageElement.currentSrc && imageElement.currentSrc != image.currentSrc) {
 				// console.log("New currentSrc:", dm.Utils.Image.getSrc(imageElement, true));
 				image.currentSrc = imageElement.currentSrc;
-				self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_INVALIDATE, image));
+
+				if(!self.numLoading()) {
+					if(invalidateAll) {
+						var invalidateSrc;
+						for(invalidateSrc in images)
+							images[invalidateSrc].invalidate();
+					}
+					self.dispatchEvent(new dm.Event(dm.ImageLoader.EVENT_INVALIDATE, image));
+				}
+
 				image.load();
 			}
 		}
 	}
 
-	this.loadImage = function(src) {
+	/*this.loadImage = function(src) {
 		var image = images[src];
 		if(image) {
 			image.load();
 		} else
-			console.warn("dm.ImageLoader: loadImage() No image found for", src);
+			console.warn("dm.ImageLoader: loadingImage() No image found for", src);
 	};
 
 	this.stopImage = function(src) {
@@ -47,17 +67,23 @@ dm.ImageLoader = function() {
 			image.stop();
 		} else
 			console.warn("dm.ImageLoader: stopImage() No image found for", src);
-	};
+	};*/
 
 	this.add = function(image, callback) {
+		if(!image.src) {
+			console.warning("dm.ImageLoader: add() No valid image");
+			return;
+		}
 		images[image.src] = new dm.ImageLoaderImage(image, callback);
-		images[image.src].addEventListener(dm.ImageLoaderImage.EVENT_LOAD, imageLoadHandler);
+		images[image.src].addEventListener(dm.ImageLoaderImage.EVENT_LOAD, imageHandler);
+		images[image.src].addEventListener(dm.ImageLoaderImage.EVENT_ERROR, imageHandler);
 	};
 
 	this.remove = function(src) {
 		var image = images[src];
 		if(image) {
-			image.removeEventListener(dm.ImageLoaderImage.EVENT_LOAD, imageLoadHandler);
+			image.removeEventListener(dm.ImageLoaderImage.EVENT_LOAD, imageHandler);
+			image.removeEventListener(dm.ImageLoaderImage.EVENT_ERROR, imageHandler);
 			images[image.src] = null;
 			delete images[image.src];
 		}
@@ -71,11 +97,18 @@ dm.ImageLoader = function() {
 		if(self.isLoading())
 			self.destroy();
 
+		if(!images) {
+			images = new Object();
+
+			if(autoStart !== false)
+				detectSrcChange();
+		}
+
 		var imageElements = element.querySelectorAll("img");
 
-		var index;
-		for(index in imageElements)
-			self.add(imageElements[index]);
+		var i;
+		for(i = 0; i < imageElements.length; i++)
+			self.add(imageElements[i]);
 	};
 
 	this.initWithHtml = function(html) {
@@ -86,9 +119,8 @@ dm.ImageLoader = function() {
 	};
 
 	this.load = function() {
-		var src;
-		for(src in images)
-			images[src].load();
+		if(autoStart == false)
+			detectSrcChange();
 	};
 
 	this.stop = function() {
@@ -101,13 +133,36 @@ dm.ImageLoader = function() {
 		var isLoading = false;
 
 		var src;
-		for(src in images)
-			if(!isLoading && images[src].getIsLoading()) {
+		for(src in images) {
+			if(!isLoading && images[src].isLoading()) {
 				isLoading = true;
 				break;
 			}
+		}
 
 		return isLoading;
+	};
+
+	this.numLoading = function() {
+		var numLoading = 0;
+
+		var src;
+		for(src in images)
+			if(images[src].isLoading())
+				numLoading++;
+
+		return numLoading;
+	};
+
+	this.numLoaded = function() {
+		var numLoaded = 0;
+
+		var src;
+		for(src in images)
+			if(!images[src].isLoading())
+				numLoaded++;
+
+		return numLoaded;
 	};
 
 	this.destroy = function() {
@@ -119,7 +174,9 @@ dm.ImageLoader = function() {
 	};
 };
 dm.ImageLoader.prototype = Object.create(dm.EventTarget.prototype);
+dm.ImageLoader.EVENT_IMAGE_LOAD = "imageload";
 dm.ImageLoader.EVENT_LOAD = "load";
+dm.ImageLoader.EVENT_ERROR = "error";
 dm.ImageLoader.EVENT_INVALIDATE = "invalidate";
 
 
@@ -135,42 +192,60 @@ dm.ImageLoaderImage = function(image, callback) {
 	this.callback = callback;
 	this.currentSrc = image.src;
 
-	var loadImage = null;
-	var isLoading = false;
+	var loadingImage = null;
+	var isLoading = image.srcset !== "";
 
-	function loadImageHandler(event) {
-		self.stop();
+	function loadingImageHandler(event) {
+		switch(event.type) {
+			case "load":
+				self.stop();
 
-		if(self.callback)
-			self.callback.call(self);
+				if(self.callback)
+					self.callback.call(self);
 
-		self.dispatchEvent(new dm.Event(dm.ImageLoaderImage.EVENT_LOAD));
+				self.dispatchEvent(new dm.Event(dm.ImageLoaderImage.EVENT_LOAD));
+				break;
+
+			case "error":
+				self.stop();
+				self.dispatchEvent(new dm.Event(dm.ImageLoaderImage.EVENT_ERROR));
+				console.error("dm.ImageLoaderImage: Error loading image", image.src, "using", event.target.src);
+				break;
+		}
 	}
 
 	this.load = function() {
 		if(image.src != self.currentSrc) {
-			if(loadImage)
+			if(loadingImage)
 				self.stop();
 
-			loadImage = new Image();
-			loadImage.addEventListener("load", loadImageHandler);
-			loadImage.src = self.currentSrc;
+			loadingImage = new Image();
+			loadingImage.addEventListener("load", loadingImageHandler);
+			loadingImage.addEventListener("error", loadingImageHandler);
+			loadingImage.src = self.currentSrc;
 			isLoading = true;
 		}
 	};
 
 	this.stop = function() {
-		if(loadImage) {
-			loadImage.removeEventListener("load", loadImageHandler);
-			loadImage.src = "";
-			loadImage = null;
+		if(loadingImage) {
+			loadingImage.removeEventListener("load", loadingImageHandler);
+			loadingImage.removeEventListener("error", loadingImageHandler);
+			loadingImage.src = "";
+			loadingImage = null;
 		}
 		isLoading = false;
 	};
 
-	this.getIsLoading = function() {
+	this.isLoading = function() {
 		return isLoading;
-	}
+	};
+
+	this.invalidate = function() {
+		this.stop();
+		isLoading = image.srcset !== "";
+	};
 };
 dm.ImageLoaderImage.prototype = Object.create(dm.EventTarget.prototype);
 dm.ImageLoaderImage.EVENT_LOAD = "load";
+dm.ImageLoaderImage.EVENT_ERROR = "error";
