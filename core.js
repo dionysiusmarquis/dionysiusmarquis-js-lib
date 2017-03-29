81a451bddfe2c03698965aa1cb8fca7a60c2fc0b
Dionysius Marquis <info@dennismarkgraf.com>
2017-03-29 15:32:16 +0200

    Removes HTMLCanvas from utils export

--- core.js
+++ core.js
@@ -1,4 +1,5 @@
 import HTMLElement from './src/js/core/HTMLElement'
 
 export * from './src/js/core/EventTarget'
+
 export {HTMLElement}
--- utils.js
+++ utils.js
@@ -1,6 +1,2 @@
-import HTMLCanvas from './src/js/utils/HTMLCanvas'
-
 export * from './src/js/utils/ImageLoader'
 export * from './src/js/utils/Utils'
-
-export {HTMLCanvas}
